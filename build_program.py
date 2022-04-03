587dec545aedd869210c30f37eed803a98c64947
Malcolm Anderson <malcolminyo@gmail.com>
2022-04-03 11:14:04 -0700

    Implemented solver for two facilities (doesn't account for facilities being built on top of each other though)

--- build_program.py
+++ build_program.py
@@ -1,34 +1,90 @@
+from typing import List, Tuple
 from pulp import const, LpProblem, LpMinimize, LpVariable, lpSum
 from parse_data import load_points, Neighborhood
 
 neighborhoods = load_points()
 
-prob = LpProblem("Single_Warehouse_Problem", LpMinimize)
+def solve_one_facility():
+    prob = LpProblem(f'Facility_Problem', LpMinimize)
 
-# Create our decision variables.
-# We need u and v (the coordinates of the warehouse)...
-u = LpVariable("Warehouse_X", lowBound=0, cat=const.LpInteger)
-v = LpVariable("Warehouse_Y", lowBound=0, cat=const.LpInteger)
+    # Create our decision variables.
+    # We need u and v (the coordinates of the warehouse)...
+    u = LpVariable(f'u', lowBound=0, cat=const.LpInteger)
+    v = LpVariable(f'v', lowBound=0, cat=const.LpInteger)
 
-# ...as well as an x and y slack variable, for each neighborhood.
-for i, nh in enumerate(neighborhoods):
-    nh.dx = LpVariable(f'Neighborhood_{i}_DistX', cat=const.LpInteger)
-    nh.dy = LpVariable(f'Neighborhood_{i}_DistY', cat=const.LpInteger)
+    # ...as well as an x and y slack variable, for each neighborhood.
+    for i, nh in enumerate(neighborhoods):
+        nh.dx = LpVariable(f'd_x{i}', cat=const.LpInteger)
+        nh.dy = LpVariable(f'd_y{i}', cat=const.LpInteger)
 
-# Create the objective function. For each neighborhood, we need the
-# neighborhood's population multiplied by its distance to the warehouse
-# location.
-prob += lpSum([nh.p * nh.dx + nh.p * nh.dy for nh in neighborhoods]), "Total_Walking_Distance"
+    # Create the objective function. For each neighborhood, we need the
+    # neighborhood's population multiplied by its distance to the facility
+    # location.
+    prob += lpSum([nh.p * nh.dx + nh.p * nh.dy for nh in neighborhoods]), "Total_Walking_Distance"
 
-# Finally, for each neighborhood, we need to add several constraints.
-for i, nh in enumerate(neighborhoods):
-    prob += nh.x - u - nh.dx <= 0, f'Neighborhood_{i}_X_UB'
-    prob += -nh.x + u - nh.dx <= 0, f'Neighborhood_{i}_X_LB'
-    prob += nh.y - v - nh.dy <= 0, f'Neighborhood_{i}_Y_UB'
-    prob += -nh.y + v - nh.dy <= 0, f'Neighborhood_{i}_Y_LB'
-    prob += -nh.dx - nh.dy + 1 <= 0, f'Neighborhood_{i}_Prevent_Build'
+    # Finally, for each neighborhood, we need to add several constraints.
+    for i, nh in enumerate(neighborhoods):
+        prob += nh.x - u - nh.dx <= 0, f'Neighborhood_{i}_X_UB'
+        prob += -nh.x + u - nh.dx <= 0, f'Neighborhood_{i}_X_LB'
+        prob += nh.y - v - nh.dy <= 0, f'Neighborhood_{i}_Y_UB'
+        prob += -nh.y + v - nh.dy <= 0, f'Neighborhood_{i}_Y_LB'
+        prob += -nh.dx - nh.dy + 1 <= 0, f'Neighborhood_{i}_Prevent_Build'
 
-prob.writeLP('SingleWarehouse.lp')
-prob.solve()
+    prob.writeLP(f'OneFacility.lp')
+    prob.solve()
 
-print(f'Optimal location is ({u.varValue}, {v.varValue})')
+    print(f'Optimal location is ({u.varValue}, {v.varValue})')
+
+def solve_two_facility():
+    prob = LpProblem('Double_Facility_Problem', LpMinimize)
+
+    # Create decision variables, again.
+    # This time, because we have two warehouses, we need (u1, v1) and (u2, v2).
+    u1 = LpVariable('u_1', lowBound=0, cat=const.LpInteger)
+    v1 = LpVariable('v_1', lowBound=0, cat=const.LpInteger)
+    u2 = LpVariable('u_2', lowBound=0, cat=const.LpInteger)
+    v2 = LpVariable('v_2', lowBound=0, cat=const.LpInteger)
+
+    # We need a copy of the neighborhoods for facility 1 and facility 2.
+    neighborhoods_1 = neighborhoods.copy()
+    neighborhoods_2 = neighborhoods.copy()
+
+    # Create the slack variables for the first facility.
+    for i, nh in enumerate(neighborhoods_1):
+        nh.dx = LpVariable(f'd_x_1_{i}', cat=const.LpInteger)
+        nh.dy = LpVariable(f'd_y_1_{i}', cat=const.LpInteger)
+
+    # Create the slack variables for the second facility.
+    for i, nh in enumerate(neighborhoods_2):
+        nh.dx = LpVariable(f'd_x_2_{i}', cat=const.LpInteger)
+        nh.dy = LpVariable(f'd_y_2_{i}', cat=const.LpInteger)
+
+    # Create the objective function.
+    neighborhoods_union = neighborhoods_1 + neighborhoods_2
+    prob += lpSum([nh.p * nh.dx + nh.p * nh.dy for nh in neighborhoods_union]), 'Total_Walking_Distance'
+
+    # Add the constraints for the first facility.
+    for i, nh in enumerate(neighborhoods_1):
+        prob += nh.x - u1 - nh.dx <= 0, f'N{i}_F1_X_UB'
+        prob += -nh.x + u1 - nh.dx <= 0, f'N{i}_F1_X_LB'
+        prob += nh.y - v1 - nh.dy <= 0, f'N{i}_F1_Y_UB'
+        prob += -nh.y + v1 - nh.dy <= 0, f'N{i}_F1_Y_LB'
+        prob += -nh.dx - nh.dy + 1 <= 0, f'N{i}_F1_Prevent_Build'
+
+    # Add the constraints for the second facility.
+    for i, nh in enumerate(neighborhoods_2):
+        prob += nh.x - u2 - nh.dx <= 0, f'N{i}_F2_X_UB'
+        prob += -nh.x + u2 - nh.dx <= 0, f'N{i}_F2_X_LB'
+        prob += nh.y - v2 - nh.dx <= 0, f'N{i}_F2_Y_UB'
+        prob += -nh.y + v2 - nh.dx <= 0, f'N{i}_F2_Y_LB'
+        prob += -nh.dx - nh.dy + 1 <= 0, f'N{i}_F2_Prevent_Build'
+
+    prob.writeLP('TwoFacility.lp')
+    prob.solve()
+
+    f1 = (u1.varValue, v1.varValue)
+    f2 = (u2.varValue, v2.varValue)
+
+    print(f'Optimal locations are {f1=}, {f2=}')
+
+solve_two_facility()
